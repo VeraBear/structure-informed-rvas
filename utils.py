0cba031302678edf7b02c63643c3e5a3ae343a0b
hilaryfinucane <hilaryfinucane@gmail.com>
2025-04-04 15:50:10 -0400

    new utils

--- utils.py
+++ utils.py
@@ -1,3 +1,32 @@
+import numpy as np
+from Bio.PDB import PDBParser
+
+def get_pairwise_distances(pdb_file):
+    parser = PDBParser(QUIET=True)
+    structure = parser.get_structure("protein", pdb_file)
+
+    ca_atoms = []
+    for model in structure:
+        for chain in model:
+            for residue in chain:
+                if 'CA' in residue:
+                    ca_atoms.append(residue['CA'].get_coord())
+    ca_atoms = np.array(ca_atoms)
+    pairwise_distances = np.sqrt(np.sum((ca_atoms[:, np.newaxis] - ca_atoms) ** 2, axis=-1))
+    return pairwise_distances
+
+def get_adjacency_matrix(pairwise_distances, radius):
+    return (pairwise_distances < radius) * 1
+
+def valid_for_fisher(contingency_table):
+    valid_columns = np.all(np.sum(contingency_table, axis=0) > 0)
+    valid_rows = np.all(np.sum(contingency_table, axis=1) > 0)
+    all_non_neg = np.all(contingency_table >= 0)
+    if valid_columns and valid_rows and all_non_neg:
+        return True
+    else:
+        return False
+
 def get_pairwise_distance_matrix(pdb_file):
     '''
     return all pairwise distances
